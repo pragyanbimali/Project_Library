7449a622e3a5bdfc328d4d23ae8e6423f913b3db
Pragyan Bimali <pragyanb02@gmail.com>
2025-06-09 20:56:02 +1000

    Create basic HTML structure and create Book Constructor and some querySelectors

---
+++ script.js
@@ -0,0 +1,18 @@
+function Book (title, author, pages, haveRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.haveRead = haveRead;
+}
+
+const buttonNewBook = document.querySelector(".add-book")
+const cardContainer = document.querySelector(".main-card-container")
+
+
+
+buttonNewBook.addEventListener("click", function() {
+    const bookList = [];
+
+
+})  
+
