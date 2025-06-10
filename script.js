d8f6ebb23c545760200540ac42661ad07b3c6d9c
Pragyan Bimali <pragyanb02@gmail.com>
2025-06-10 21:52:38 +1000

    Add a function to render the books in the array in the page inside the main container

--- script.js
+++ script.js
@@ -17,6 +17,42 @@ const closeBtn = document.querySelector(".close-dialog-btn");
 // We first create an empty array to store the books we create 
 const bookList = [];
 
+function renderBooks() {
+    cardContainer.textContent = "";
+    bookList.forEach(book => {
+        const bookCard = document.createElement("div");
+        bookCard.setAttribute("class", "book-card");
+        
+        const titleDiv = document.createElement("div");
+        titleDiv.setAttribute("class", "title-div");
+        titleDiv.textContent = `Title: ${book.title}`;
+
+        const authorDiv = document.createElement("div");
+        authorDiv.setAttribute("class", "author-div");
+        authorDiv.textContent = `Author: ${book.author}`;
+
+        const pagesDiv = document.createElement("div");
+        pagesDiv.setAttribute("class", "pages-div");
+        pagesDiv.textContent = `Pages: ${book.pages}`;
+
+        const haveReadDiv = document.createElement("div");
+        haveReadDiv.setAttribute("class", "haveRead-div");
+        haveReadDiv.textContent = "Have Read?: "
+        if (haveRead) {
+            haveReadDiv.textContent += "Have Read"
+        } else {
+            haveReadDiv.textContent += "Have NOT Read"
+        }
+
+        bookCard.appendChild(titleDiv);
+        bookCard.appendChild(authorDiv);
+        bookCard.appendChild(pagesDiv);
+        bookCard.appendChild(haveReadDiv);
+
+        cardContainer.appendChild(bookCard);
+    })
+}
+
 
 // When we click the "Add New Book" button.....
 buttonNewBook.addEventListener("click", function() {
@@ -41,6 +77,7 @@ bookDetailForm.addEventListener("submit", function(e) {
     const bookInstance = new Book (bookTitle, authorName, totalPages, haveReadValue);
 
     bookList.push(bookInstance);
+    renderBooks();
 
     console.log(bookList);
 
