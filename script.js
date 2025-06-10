fae3c12a87016ea02196d15b9cda308f08cc2cb4
Pragyan Bimali <pragyanb02@gmail.com>
2025-06-10 21:27:44 +1000

    Refactor HTML to add dialog and forms then add dialogs responsiveness to JS

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 // Defined the constructor function for the books instances.
 function Book (title, author, pages, haveRead) {
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -9,55 +10,43 @@ function Book (title, author, pages, haveRead) {
 
 const buttonNewBook = document.querySelector(".add-book");
 const cardContainer = document.querySelector(".main-card-container");
+const dialog = document.querySelector(".dialog");
+const bookDetailForm = document.querySelector(".bookDetailForm");
+const closeBtn = document.querySelector(".close-dialog-btn");
 
 // We first create an empty array to store the books we create 
 const bookList = [];
 
+
 // When we click the "Add New Book" button.....
 buttonNewBook.addEventListener("click", function() {
-    // First we create the form element(s) to prompt the user for input
-    const bookDetailForm = document.createElement("form");  // Creating the main form element
-    bookDetailForm.setAttribute("class", "book-form"); // Giving it a class for CSS purposes of course
+    dialog.showModal();
+    closeBtn.addEventListener("click", () => dialog.close())
+})
 
-    const titleLabel = document.createElement("label"); // Then create a label element
-    titleLabel.setAttribute("for", "title");            //  for the title input 
-    titleLabel.textContent("Title: ");
+bookDetailForm.addEventListener("submit", function(e) {
+    e.preventDefault();
 
-    const titleInput = document.createElement("input"); // Then create the actual input box which we link to the label element
-    titleInput.setAttribute("type", "text");            // Type of input is text
-    titleInput.setAttribute("id", "title");             // We give it an ID
-    titleInput.setAttribute("name", "bookTitle");       //  and a name that we can use in the backend later on
+    const bookTitle = e.target.elements.bookTitle.value;
+    const authorName = e.target.elements.bookAuthor.value;
+    const totalPages = e.target.elements.bookPages.value;
+    let haveReadValue = 0;
+    if (e.target.elements.bookhaveRead.checked) {
+        haveReadValue = 1;
+    } else {
+        haveReadValue = 0;
+    }
 
-    // Similarly we then create the other elements of the form
+    const bookInstance = new Book (bookTitle, authorName, totalPages, haveReadValue);
 
-    // For the Author 
-    const authorLabel = document.createElement("label"); 
-    titleLabel.setAttribute("for", "author");            
-    titleLabel.textContent("Author: ");
+    bookList.push(bookInstance);
 
-    const authorInput = document.createElement("input"); 
-    titleInput.setAttribute("type", "text");            
-    titleInput.setAttribute("id", "author");             
-    titleInput.setAttribute("name", "bookAuthor");  
-    
-    // For the Pages
-    const pagestLabel = document.createElement("label"); 
-    titleLabel.setAttribute("for", "pages");            
-    titleLabel.textContent("Pages: ");
+    console.log(bookList);
 
-    const pagesInput = document.createElement("input"); 
-    titleInput.setAttribute("type", "number");            
-    titleInput.setAttribute("id", "pages");             
-    titleInput.setAttribute("name", "bookPages"); 
+    dialog.close();
+})
 
-    // For the haveRead, we create a Checkbox input type
-    const haveReadLabel = document.createElement("label"); 
-    titleLabel.setAttribute("for", "haveRead");            
-    titleLabel.textContent("Have Read? ");
 
-    const haveReadInput = document.createElement("input"); 
-    titleInput.setAttribute("type", "checkbox");            
-    titleInput.setAttribute("id", "haveRead");             
-    titleInput.setAttribute("name", "bookhaveRead");
-})  
+
+
 
